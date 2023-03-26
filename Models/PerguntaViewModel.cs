f8e8d9c955443cc508cf935a975b00b8cf8c4a57
Renan Martins <renanmartins1999@hotmail.com>
2023-03-25 22:45:18 -0300

    [FEAT] Adicionando modelo

---
+++ Models/PerguntaViewModel.cs
@@ -0,0 +1,9 @@
+namespace Quiz_Programacao.Models
+{
+    public class PerguntaViewModel
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }        
+        public bool Aprovado { get; set; }
+    }
+}
