b5d7e0c59202b91cfc234e8c6c31047191d31ca5
Renan Martins <renanmartins1999@hotmail.com>
2023-03-26 19:34:25 -0300

    [FEAT] Adicionando propriedade no modelo Pergunta

--- Models/PerguntaViewModel.cs
+++ Models/PerguntaViewModel.cs
@@ -3,6 +3,7 @@ namespace Quiz_Programacao.Models
     public class PerguntaViewModel
     {
         public int Id { get; set; }
-        public string Descricao { get; set; }        
+        public string Descricao { get; set; }
+        public bool Verdadeiro { get; set; }
     }
 }
