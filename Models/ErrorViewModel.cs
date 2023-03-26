c0e955dc550486ac4ca40b8f713cf58af4fe3573
Renan Martins <renanmartins1999@hotmail.com>
2023-03-25 21:33:22 -0300

    Inicializando repositório

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Quiz_Programacao.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
